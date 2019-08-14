81426cb419f9cc694127e06d0d3fac42f9269510
Lars Kempa <kempa@it-easy.de>
2019-08-14 15:30:53 +0200

    TryCatch

--- AppVeyorTest/Form1.vb
+++ AppVeyorTest/Form1.vb
@@ -9,7 +9,12 @@ Public Class Form1
 
     Public Shared Function TeileDurch(Dividend As Decimal, Divisor As Decimal) As Decimal
         Dim result As Decimal
-        result = Dividend / Divisor
+        Try
+            result = Dividend / Divisor
+        Catch ex As Exception
+
+        End Try
+
         Return result
     End Function
 End Class
